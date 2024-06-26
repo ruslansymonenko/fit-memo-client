a7fd4614471943651401cf4d8b1ad3464cb50cda
Ruslan Symonenko <symonenkors@gmail.com>
2024-06-26 16:22:48 +0300

    first commit

---
+++ src/consts/app.consts.ts
@@ -0,0 +1,1 @@
+export const SITE_NAME = 'Fit Memo';
---
+++ src/screens/presentation/Presentation.tsx
@@ -0,0 +1,8 @@
+import { FC } from 'react';
+import '@/app/globals.scss';
+
+const Presentation: FC = () => {
+  return <div className="">Presentation</div>;
+};
+
+export default Presentation;
